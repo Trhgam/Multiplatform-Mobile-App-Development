94efd645c8e446ded87f175e385886013908dc8b
Tran Hong Gam <honggamdx2@gmail.com>
2026-09-21 12:08:26 +0700

    feat: ex 6

--- LittleLemonApp/components/WelcomeScreen.tsx
+++ LittleLemonApp/components/WelcomeScreen.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { ScrollView, Text, StyleSheet } from "react-native";
 
 export default function WelcomeScreen() {
   return (
-    <View style={styles.container}>
+    <ScrollView indicatorStyle="white" style={styles.container}>
       <Text style={styles.headerText}>Welcome to Little Lemon</Text>
       <Text style={styles.subheadingText}>
         Little Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would
-        love to hear more about your experience with us!
+        and classic cocktails in a lively but casual environment. We would love
+        to hear more about your experience with us!
       </Text>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -18,20 +18,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#495E57",
-    paddingTop: 40,
-    paddingHorizontal: 24,
   },
   headerText: {
-    fontSize: 32,
+    padding: 40,
+    fontSize: 50,
     color: "#EDEFEE",
     textAlign: "center",
-    marginBottom: 40,
-    fontWeight: "bold",
   },
   subheadingText: {
-    fontSize: 20,
+    fontSize: 38,
+    padding: 20,
+    marginVertical: 8,
     color: "#EDEFEE",
     textAlign: "center",
-    lineHeight: 28,
   },
 });
